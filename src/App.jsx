7143680a3ace482099826c0dc77527b65279d407
DevSujal <nimjesujal2004@gmail.com>
2024-07-12 20:06:37 +0530

    Update page layout and component styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="bg-gray-900 relative w-screen h-screen">
+    <div className="bg-gray-900 relative w-screen h-full">
       <Outlet />
     </div>
   );
